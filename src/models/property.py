dbfb8e4c99563d550733f7988f0c13f73a1510e8
Julio Boll <juliocesarboll@gmail.com>
2022-01-07 20:38:14 -0300

    Property Model

---
+++ src/models/property.py
@@ -0,0 +1,9 @@
+class Property:
+    sell_value = None
+    rent_value = None
+    player_hold = None
+
+
+    def __init__(self, sell, rent):
+        self.sell_value = sell
+        self.rent_value = rent

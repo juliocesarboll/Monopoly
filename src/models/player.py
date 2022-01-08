eea120b622bca736cfbd9bc41960baa6ef3a86d9
Julio Boll <juliocesarboll@gmail.com>
2022-01-08 14:20:24 -0300

    Refinando Models

--- src/models/player.py
+++ src/models/player.py
@@ -8,5 +8,13 @@ class Player:
     def __init__(self, name, behavior):
         self.name = name
         self.money = 300
-        self.behavior = behavior
+        if behavior == 0:
+            self.behavior = 'Compulsivo'
+        elif behavior == 1:
+            self.behavior = 'Exigente'
+        elif behavior == 2:
+            self.behavior = 'Cauteloso'
+        elif behavior == 3:
+            self.behavior = 'Aleatorio'
+            
         self.list_property = []
--- src/models/property.py
+++ src/models/property.py
@@ -1,9 +1,12 @@
 class Property:
     sell_value = None
     rent_value = None
+    bonus_value = None
     player_hold = None
 
 
-    def __init__(self, sell, rent):
+    def __init__(self, sell, rent, bonus=None, hold=None):
         self.sell_value = sell
         self.rent_value = rent
+        self.bonus_value = bonus
+        self.player_hold = hold
